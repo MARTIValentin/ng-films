e3e7e83d778a0ac57868b3973da062d0fa35debf
MARTI Valentin <valentin.marti@hotmail.fr>
2022-06-08 16:22:01 +0200

    Nouveau

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TitreComponent } from './composants/titre/titre.component';
+import { SpinnerComponent } from './composants/spinner/spinner.component';
+import { SeriesComponent } from './composants/series/series.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitreComponent,
+    SpinnerComponent,
+    SeriesComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule
   ],
